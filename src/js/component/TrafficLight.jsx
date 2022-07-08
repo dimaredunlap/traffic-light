8298866dcc2c1cfef06da42b7afc28345b191b4d
dimaredunlap <dimaredunlap@gmail.com>
2022-07-08 00:57:18 +0000

    corrected project

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -7,19 +7,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const TrafficLight = () => {
 	const [ selectedColor, setSelectedColor] = useState("");
 	return (
-		<div className="traffic-light">
-			<div
-				onClick={() => setSelectedColor("red")}
-				className={"light red"+((selectedColor === "red") ? "glow" : "")
-			}></div>
-			<div
-				onClick={() => setSelectedColor("yellow")}
-			 	className={"light yellow"+((selectedColor === "yellow") ? "glow" : "")
-			}></div>
-			<div
-				onClick={() => setSelectedColor("green")}
-			 	className={"light green"+((selectedColor === "green") ? "glow" : "")
-			}></div>
+		<div className="container">
+			<div className="cable"></div>
+			<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={"light red"+((selectedColor === "red") ? "light red glowRed" : "light red")
+				}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={"light yellow"+((selectedColor === "yellow") ? "light yellow glowYellow" : "light yellow")
+				}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={"light green"+((selectedColor === "green") ? "light green glowGreen" : "light green")
+				}></div>
+			</div>
 		</div>
 	);
 };
